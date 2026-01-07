a5249e476754664828b0a9b7127628a80b5058b0
stefanhorus <horus_nfs@yahoo.com>
2026-01-07 17:37:50 +0200

    Add FREE discount code with 100% discount

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -32,6 +32,7 @@ function CheckoutContent() {
     
     // Coduri de discount simple (poți extinde cu o bază de date sau API)
     const validCodes: { [key: string]: number } = {
+      "FREE": 100,
       "WELCOME10": 10,
       "SAVE20": 20,
       "ZOOMOUT15": 15,
